66ea8ac7ac047bc9ecd1ca2ff1b51abea955fc58
Ragnar20-03 <roshanpp20@gmail.com>
2023-09-09 19:52:12 +0530

    Recurrsion in python

---
+++ Exeception_Handling/tryDemo6.py
@@ -0,0 +1,29 @@
+
+
+def main():
+    try:
+        print("Enter First number ")
+        no1 = int(input())
+    
+        print("Enter Second Number")
+        no2 = int(input())
+    
+    
+        Ans = 0 
+        Ans = no1 / no2
+        
+    except ZeroDivisionError as zobj :
+        print(zobj)
+        print("Divide by zero is Not Allowed")
+        return
+    except Exception as eobj :
+        print(eobj)
+        print("Divide by zero is Not Allowed")
+        return
+    
+
+    print("Division is : " , Ans)
+
+
+if __name__ == "__main__" :
+    main()
