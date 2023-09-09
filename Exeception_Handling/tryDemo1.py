b098c72e6c17fa570e1e6439d4c373852064bccc
Ragnar20-03 <roshanpp20@gmail.com>
2023-09-09 18:48:49 +0530

    Execption Handling

---
+++ Exeception_Handling/tryDemo1.py
@@ -0,0 +1,18 @@
+
+
+def main():
+    print("Enter First number ")
+    no1 = int(input())
+
+    print("Enter Second Number")
+    no2 = int(input())
+
+
+    Ans = 0 
+    Ans = no1 / no2
+
+    print("Division is : " , Ans)
+
+
+if __name__ == "__main__" :
+    main()
